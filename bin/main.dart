131c57c03642c0d2a86529e19dd5bdeed47da3e4
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-04-10 15:51:49 +0200

    Changes to be committed: 	modified:   bin/main.dart 	new file:   lib/cli/preview.dart 	new file:   lib/cli/prompts.dart 	modified:   lib/config/constants/flutrix_constanst.dart 	new file:   lib/config/writer/configs_writer.dart 	new file:   lib/flutter/flutter_creator.dart 	new file:   lib/flutter/project_builder.dart 	new file:   lib/models/project_config_model.dart 	new file:   lib/pub/pub_pkg_insttaller.dart 	new file:   lib/pub/pub_search.dart 	modified:   pubspec.yaml

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,43 @@
+import 'dart:io';
+
+import 'package:flutrix/config/constants/flutrix_constanst.dart';
+
 void main(List<String> arguments) async {
-    print('\n╔══════════════════════════════════════════════════╗');
-  print('║     Flutter Architecture Generator CLI Tool      ║');
-  print('╚══════════════════════════════════════════════════╝\n');
+  print('''
+╔════════════════════════════════════════════════════╗
+║            ${FlutriXConstanst.cliName}               ║
+║            Version: ${FlutriXConstanst.cliVersion}    ║
+║            Author: ${FlutriXConstanst.cliAuthor}      ║
+║            License: ${FlutriXConstanst.cliLicense}    ║
+╚════════════════════════════════════════════════════╝
+''');
 
+  // case one phase user use old configuration json file.
+  final oldConfig = await loadConfig();
+  if (oldConfig != null) {
+    stdout.write('\nDo you want to use the old configuration? (y/n): ');
+    final usreResponse = stdin.readLineSync()?.toLowerCase();
 
-  
+    switch (usreResponse) {
+      case 'y':
+      //  await createProject(oldConfig);
+        break;
+      case 'n':
+     //   await createProject(null);
+        break;
+      default:
+        print('Invalid response. Please enter "y" or "n".');
+    }
+  } else {
+   // await createProject(null);
+  }
+
+  // case 2 phase user use interactive CLI.
+  final config = askProjectDetails();
+
+  await saveConfigToJson(config);
+
+  await previewProjectArchitecture(config);
+
+  await confirmAndGenerateProject(config);
 }
--- lib/config/constants/flutrix_constanst.dart
+++ lib/config/constants/flutrix_constanst.dart
@@ -1,6 +1,18 @@
 class FlutriXConstanst {
   static const String cliName = 'FlutriX'; 
 
+  static const String cliVersion = '0.0.1';
 
-  
+  static const String cliDescription = 'Dart CLI Tool for Generating Base Mobile App Architecture: Clean Architecture | Feature-Based Architecture | MVC | MVVM | MVP | MVI';
+
+  static const String cliAuthor = 'MostafaSensei106';
+
+  static const String cliRepository = 'https://github.com/MostafaSensei106/Flutter-Architecture-Generator';
+
+  static const String cliAuthorGitHub = 'https://github.com/MostafaSensei106';
+
+  static const String cliLicense = 'MIT';
+
+
+
 }
