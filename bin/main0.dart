a62003b6a2033c83c1abe036750a03dc42101105
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-04-10 15:30:43 +0200

    new file:   bin/main0.dart 	modified:   pubspec.yaml
    Changes not staged for commit:
    	deleted:    bin/main0.dart

---
+++ bin/main0.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) async {
+    print('\n╔══════════════════════════════════════════════════╗');
+  print('║     Flutter Architecture Generator CLI Tool      ║');
+  print('╚══════════════════════════════════════════════════╝\n');
+
+
+  
+}
